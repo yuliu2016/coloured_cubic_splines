d88170cb6fc2fe7e73e34aba8e9a48ad52b9fd59
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2019-03-13 10:12:09 -0400

    add quintic spline

--- coloured_quintic_splines.py
+++ coloured_quintic_splines.py
@@ -30,10 +30,41 @@ class CubicSpline:
         return 6 * self._a
 
 
-def spline_seg(p0, m0, p1, m1):
-    pts = list(zip(p0, m0, p1, m1))
-    return CubicSpline(*pts[0]), CubicSpline(*pts[1])
+def norm(p):
+    m = math.hypot(p[0], p[1])
+    if m == 0:
+        return 0, 0
+    return p[0]/m, p[1]/m
 
+class QuinticSpline:
+    def __init__(self, p0, m0, dm0, p1, m1, dm1):
+        self._a = -6 * p0 - 3 * m0 - 0.5 * dm0 + 0.5 * dm1 - 3 * m1 + 6 * p1
+        self.b = 15 * p0 + 8 * m0 + 1.5 * dm0 - dm1 + 7 * m1 - 15 * p1
+        self.c = -10 * p0 - 6 * m0 - 1.5 * dm0 + 0.5 * dm1 - 4 * m1 + 10 * p1
+        self.d = 0.5 * dm0
+        self.e = m0
+        self.f = p0
+
+    def __getitem__(self, t):
+        return self._a * t ** 5 + self.b * t ** 4 + self.c * t ** 3 +  self.d * t ** 2 +  self.e * t +  self.f
+
+    def v(self, t):
+        return 5 * self._a * t ** 4 + 4 * self.b * t ** 3 + 3 * self.c * t ** 2 + 2 * self.d * t + self.e
+
+    def a(self, t):
+        return 20 * self._a * t ** 3 + 12 * self.b * t ** 2 + 6 * self.c * t + 2 * self.d
+
+    def j(self, t):
+        return 60 * self._a * t ** 2 + 24 * self.b * t + 6 * self.c
+
+
+# def spline_seg(p0, m0, p1, m1):
+#     pts = list(zip(p0, m0, p1, m1))
+#     return CubicSpline(*pts[0]), CubicSpline(*pts[1])
+
+def spline_seg(p0, m0, p1, m1):
+    pts = list(zip(p0, m0, m0, (0, 0), m1, (0, 0)))
+    return QuinticSpline(*pts[0]), QuinticSpline(*pts[1])
 
 display.fill((255, 255, 255))
 
@@ -101,7 +132,6 @@ def draw(c, d, tp):
             pygame.draw.circle(display, fc, t, 2)
         else:
             pygame.draw.line(display, fc, f, t, 4)
-            print(t, w)
         display.set_at((t[0] + w[0], t[1] - w[1]), fc)
         display.set_at((t[0] - w[0], t[1] + w[1]), fc)
 
@@ -122,7 +152,7 @@ draw((0, 128, 255), False, spline_seg((100, 0), (100, 0), (330, 18), (100, 0)))
 
 #RIGHT
 
-draw((255, 128, 0), False, spline_seg((100, 75), (200, 0), (390, 220), (-700, 400))) # far rocket
+draw((255, 128, 0), False, spline_seg((100, 75), (200, 0), (390, 220), (-525, 300))) # far rocket
 draw((255, 0, 128), True, spline_seg((390, 220), (175, -100), (445, 50), (0, -200))) # far rocket to cargo ship
 
 draw((128, 0, 255), False, spline_seg((100, 75), (200, 0), (360, 200), (0, 400))) # mid rocket
